3fccc24c9942c66ea27adc81756c4fcfa15c4808
Giorgi <gasanidze@tbcbank.com.ge>
2022-08-10 15:15:27 +0400

    nx micro front-end

---
+++ apps/search/src/app/remote-entry/entry.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-mfe-search-entry',
+  template: `<nx-mfe-nx-welcome></nx-mfe-nx-welcome>`,
+})
+export class RemoteEntryComponent {}
